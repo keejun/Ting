8b9c34eb94e483f0f0f1d1c4b7e04a5fe043f268
keejunliu <keejunliu@gmail.com>
2013-02-17 14:39:30 +0800

    add sth
    do sth

---
+++ Music/src/play/music/action/Deletelove.java
@@ -0,0 +1,31 @@
+package play.music.action;
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import play.music.util.*;
+import play.music.dao.Musicdao;
+import play.music.dao.impl.Musicdaoimpl;
+import play.music.entity.Music;
+import org.junit.Test;
+public class Deletelove extends HttpServlet {
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+			request.setCharacterEncoding("utf-8");
+			response.setContentType("text/html"); 
+			String name=request.getParameter("name");
+			String musicid=request.getParameter("musicid");
+			Musicdao dao =new Musicdaoimpl();
+			try{
+			   dao.dislove(name, musicid);
+			   System.out.println("删除成功");
+			  } catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("系統出錯deletelove");
+				}
+			
+		}
+
+	}
